32a395d3cfd2dedbf428e57b2f2f6b7f385ced41
mousse90 <paulmousse@yahoo.com>
2023-04-09 23:15:51 +0100

    Our first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const _ = require("underscore");
+//1st core module ? No
+//2nd folder ?  or file --> No
+//3rd ? inside the node_modules
+const result = _.contains([1, 2, 3], 2);
+console.log(result);
